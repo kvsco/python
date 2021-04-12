3e6fa477366b6edfc1fcf903a16e8f9c0a1ce803
kvsco <nngjk@naver.com>
2021-04-12 17:10:05 +0900

    add 2-1,2
    basic

---
+++ main.py
@@ -0,0 +1,27 @@
+# 정수형
+a = 123
+b = -178
+# 실수형
+c = 1.2
+d = -3.45
+# 8진수 , 16진수
+e = 0o177
+f = 0x8ff
+print(f"a = {a}, type = {type(a)}")
+print(f"a = {b}, type = {type(b)}")
+print(f"a = {c}, type = {type(c)}")
+print(f"a = {d}, type = {type(d)}")
+print(f"a = {e}, type = {type(e)}")
+print(f"a = {f}, type = {type(f)}")
+# 사칙연산
+a, b = 3, 4
+print(f"a + b = {a+b}")
+print(f"a * b = {a*b}")
+print(f"a / b = {a/b}")
+# 제곱근
+print(f"a ** b = {a**b}")
+# 나머지
+print(f"b % a = {b%a}")
+print(f"a % b = {a%b}")
+# 나눗셈 몫
+print(f"b // a = {b//a}")
