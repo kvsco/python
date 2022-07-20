da63ca145c6ef42b547c63d6690fe986679eaec0
kvsco <nngjk@naver.com>
2022-07-20 23:28:06 +0900

    update untitle2, add untile3

--- 제어 flow/untitle2.py
+++ 제어 flow/untitle2.py
@@ -57,3 +57,73 @@ def say_hello():
 f = say_hello()
 print(f"f = {f}")
 
+num: int = 10
+def add_num(a: int, b: int) -> int:
+    return a+b
+
+r1 = add_num(10,20)
+r2 = add_num('a', 'b')
+print(r1)
+print(r2)
+
+def menu(entree, drink, dessert):
+    print(entree)
+    print(drink)
+    print(dessert)
+
+menu('beef','beer','ice')
+
+# 명시적으로 파라미터 인수를 지정하는방법
+menu(drink='ice', entree='beef', dessert='beer')
+
+# default 인수
+def menu2(entree='beef', drink='wine', dessert='icecream'):
+    print(entree, drink, dessert)
+
+menu2('chicken', drink="water")
+
+# default 인수 사용시 주의점, list, dict 를 사용하지 않는다. python 의 특성 -> 참조 반환 object // 참조 반환 Obj를 꼭 써야한다면 None 으로 초기화
+def func1(x, l=[]):
+    l.append(x)
+    return l
+
+y = [1,2,3]
+r = func1(100, y)
+print(r)
+
+def func2(x, l=None):
+    if l is None:
+        l = []
+    l.append(x)
+    return l
+
+# 여러개 인자값 사용 ---> *args (tuple)
+def say_something(*args):
+    print(args)
+    for arg in args:
+        print(arg)
+
+say_something('hi','mike','nance')
+
+# key word 인수 --> **kwargs (dict)
+def k_menu(**kwargs):
+    print(kwargs)
+    for k,v in kwargs.items():
+        print(f"k : {k} , v : {v}")
+
+k_menu(entree='beef', dessert='icecream', drink='coffee')
+
+menu_dict = {
+    'entree' : 'beef',
+    'drink' : 'juice',
+    'dessrt' : 'cake'
+}
+k_menu(**menu_dict)  # ** 의미 : dictionary -> unpacking 된다.
+
+# 위치인수 args, kwargs 종합
+def final_menu(name, *args, **kwargs):  # 순서 중요 * -> **
+    print(f"name : {name}")
+    print(f"apitzer : {args}")
+    print(f"menu : {kwargs}")
+
+final_menu("yong", 'choco', 'water', **menu_dict)
---
+++ 제어 flow/untitle3.py
@@ -0,0 +1,73 @@
+# doc string 함수의 설명
+def example_func(param1, param2):
+    """
+    Example function 의 기능은 'ㅇㅇㅇ'입니다.
+
+    Args:
+        param1: asdasd
+        param2: asdasd
+    Returns:
+        bool: asdasd
+    """
+    return False
+
+print(example_func.__doc__)
+
+# inner function
+def outer(a, b):
+
+    def plus(c, d):  # 내부에서 여러번 호출되어 사용될 경우 사용
+        return c+d
+
+    r = plus(a, b)
+    r2 = plus(b, a)
+    r3 = plus(a+1, b+1)
+    print(r)
+
+outer(1, 2)
+
+# 클로저
+def outer1(a, b):
+    def inner():
+        return a + b
+
+    return inner
+
+f = outer1(3, 4) # 인수만 넣어두고 나중에 function 실행
+print(f)
+print(f())
+
+def circle_area_func(pi):
+    def area(rad):
+        return pi * rad * rad
+
+    return area
+
+ca1 = circle_area_func(3.14)
+ca2 = circle_area_func(3.141592)
+
+print(ca1(10))
+print(ca2(10))
+
+# 데코레이터 -> 함수에 대한 전후 처리를 반복적으로 하기 위함.
+def print_info(func):
+    def wrapper(*args, **kwargs):
+        print(" start")
+        print(f" func: {func.__name__}")
+        print(f" args: {args}")
+        print(f" kwargs: {kwargs}")
+        res = func(*args, **kwargs)
+        print(" end")
+        return res
+    return wrapper
+
+@print_info
+def add_num(a, b):
+    return a + b
+
+@print_info
+def sub_num(a, b):
+    return a - b
+
+r = add_num(10,20)
+r2 = sub_num(30,20)
