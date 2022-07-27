40a47d8076d2deed28e0b400a6a1421001cf1542
kvsco <nngjk@naver.com>
2022-07-27 23:16:30 +0900

    update class,static, speical method

--- 객체,클래스/클래스변수.py
+++ 객체,클래스/클래스변수.py
@@ -30,3 +30,46 @@ d.add_word("add A")
 d.add_word("add B")
 print(d.words)
 
+# =================== 클래스 메소드, 스태틱 메소드 ===================== #
+class Human:
+    # class variable
+    kind = "king"
+
+    def __init__(self):
+        self.x = 100
+
+    @classmethod
+    def what_is_ur_name(cls):
+        return cls.kind
+
+    @staticmethod
+    def about():
+        print("about human") # class 외부에 독립적으로 있는 메소드처럼 동작, self 없음 단, 클래스 내부에 사용되는 이유는 : human클래스에 연관된 메소드의 의미를 담고있다.
+
+h1 = Human()
+h2 = Human
+print(h1)  # object = 객체 = human class 의 instance
+print(h2)  # human class
+
+# print(h1.x)
+# print(h2.x)  # 접근x 에러
+print(h1.what_is_ur_name())
+print(h2.what_is_ur_name())  # object를 만들지 않아도 접근할 수 있다.
+h2.about()
+h1.about()
+
+# =================== 특수 메소드 ===================== #
+class Word:
+    # __ 언더바 __ 에 해당하는 메소드 = 특수 메소드
+    def __init__(self, text):
+        self.word = text
+
+    def __str__(self):  # 문자열 호출시 print()
+        return " str sucess"
+
+    def __len__(self):  # len() 호출시
+        return len(self.word)
+
+w = Word("test_text")
+print(w)
+print(len(w))  # = len(w.word)
