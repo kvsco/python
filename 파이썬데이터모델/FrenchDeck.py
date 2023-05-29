9d7f5622489410db6e4b7d7ad082ac4a943757b4
kvsco <nngjk@naver.com>
2023-05-29 18:09:14 +0900

    [Python magic method] FrenchDeck

---
+++ 파이썬데이터모델/FrenchDeck.py
@@ -0,0 +1,28 @@
+import collections
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2,11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, pos):
+        return self._cards[pos]
+
+####
+if __name__ == "__main__":
+    f_card = Card('7', 'diamonds')
+    print(f_card)
+
+    deck = FrenchDeck()
+    print(len(deck))
+    print(deck[0])
+    print(deck[1])
+    print(deck[2])
+    print(deck[-1])
